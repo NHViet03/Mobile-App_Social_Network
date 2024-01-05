a564799636fedc622ec7cad29d94c7b6478063af
AnhHong <hongoni8323@gmail.com>
2024-01-05 14:09:22 +0700

    Hong code UI LogGin

---
+++ client/components/HongCode.js
@@ -0,0 +1,12 @@
+import { Pressable, View } from "react-native";
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+
+const HongCode = () => {
+    return
+    <View>
+        <Text>Day la code 1</Text>
+    </View>
+};
+
+export default HongCode;
