03ebf8c357cb52e627f45c4edef11194b4a78369
Ho Quang Dinh <21520190@gm.uit.edu.vn>
2024-01-05 14:38:36 +0700

    Update Model Message

---
+++ client/components/DinhCode.js
@@ -0,0 +1,14 @@
+import { Pressable, View } from "react-native";
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+
+const DinhCode = () => {
+    return
+    <View>
+        <Text>UI check code 1</Text>
+        <Text>Day la code 2</Text>
+        <Text>UI check code 3</Text>
+    </View>
+};
+
+export default DinhCode;
